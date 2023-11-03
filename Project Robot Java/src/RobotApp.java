fc720942e615ec88f0dc13c038491b6ed7c70124
Charles Ricky Barnabas <110974630+CharlesBarnabas07@users.noreply.github.com>
2023-11-04 02:22:36 +0700

    Tugas Robot java sudah di push

--- Project Robot Java/src/RobotApp.java
+++ Project Robot Java/src/RobotApp.java
@@ -9,66 +9,60 @@ public class RobotApp {
         new RobotApp();
     }
 
-    /*
-     * Perintah Soal:
-     * Anggap 'o' adalah sebuah robot yang berada didalam area permainan.
-     * Buat robot dapat bergerak ke kiri/kanan/atas/bawah sesuai jumlah langkah yang diinstruksikan.
-     * Format instruksi: {d=kanan/a=kiri/w=atas/s=bawah}{jumlah langkah} dan 'x' untuk keluar aplikasi *case sensitive
-     * Robot tidak boleh keluar dari area permainan dan buat pesan kesalahannya.
-     * Buat pesan kesalahan jika pengguna tidak menulis instruksi dengan benar.
-     * Lanjut pada perintah soal dibawah.
-     */
-
     private Layout layout;
     private Robot robot;
     private Scanner scanner;
+
     public RobotApp() {
-        // contoh konfigurasi (inisiasi object layout) area permainan: X = 10, Y = 10, icon area yang tidak ditempati robot adalah '*'
         this.layout = new Layout(10, 10, '*');
+        this.robot = new Robot('o', new Position(1, 1)); // Posisi awal robot: 1,1
         this.scanner = new Scanner(System.in);
-        String instruction = "";
+
         System.out.println("-------- Permainan Dimulai --------");
-        draw();
-        do{
+
+        String instruction;
+        do {
+            draw();
             instruction = waitInstruction();
-            String direction = instruction.substring(0, 1);
-            int step = 0;
-            if (!(instruction.equals("x"))) {
-                step = Integer.parseInt(instruction.substring(1)); 
-            }
-
-            if (direction.equals("d")) {
-                robot.setPosition(new Position(robot.getPosition().getX() + step, robot.getPosition().getY()));
-            } else if (direction.equals("a")) {
-                robot.setPosition(new Position(robot.getPosition().getX() - step, robot.getPosition().getY()));
-            } else if (direction.equals("w")) {
-                robot.setPosition(new Position(robot.getPosition().getX(), robot.getPosition().getY() - step));
-            } else if (direction.equals("s")) {
-                robot.setPosition(new Position(robot.getPosition().getX(), robot.getPosition().getY() + step));
-            } else {
-                System.out.println("Instruksi tidak dikenali, permainan selesai");
-            }
-
-            if (robot.getPosition().getX() < 0 || robot.getPosition().getX() > layout.getMaxX() - 1 || robot.getPosition().getY() < 0 || robot.getPosition().getY() > layout.getMaxY() - 1) {
-                System.out.println("Robot keluar dari area permainan, permainan selesai");
-                break;
-            }
-
-            System.out.println("------ Posisi Terbaru ------");
-            //set robot position and erase previous position
-            for (int y = 0; y < layout.getMaxY(); y++) {
-                for (int x = 0; x < layout.getMaxX(); x++) {
-                    if (x == robot.getPosition().getX() && y == robot.getPosition().getY()) {
-                        System.out.print(robot.getIcon());
-                    } else {
-                        System.out.print(layout.getArea()[x][y]);
-                    }
-                }
-                System.out.println();
-            }
-
-        }while(!instruction.equals("x"));
+            processInstruction(instruction);
+        } while (!robotIsOutside() && !instruction.equals("x"));
         System.out.println("-------- Permainan Selesai --------");
+    }
+
+    private void processInstruction(String instruction) {
+        char direction = instruction.charAt(0);
+        int steps = Integer.parseInt(instruction.substring(1));
+
+        switch (direction) {
+            case 'd':
+                moveRobot(steps, 0);
+                break;
+            case 'a':
+                moveRobot(-steps, 0);
+                break;
+            case 'w':
+                moveRobot(0, -steps);
+                break;
+            case 's':
+                moveRobot(0, steps);
+                break;
+            default:
+                System.out.println("Instruksi tidak valid, permainan selesai");
+        }
+    }
+
+    private void moveRobot(int deltaX, int deltaY) {
+        Position currentPosition = robot.getPosition();
+        int newX = currentPosition.getX() + deltaX;
+        int newY = currentPosition.getY() + deltaY;
+
+        robot.setPosition(new Position(newX, newY));
+    }
+
+    private boolean robotIsOutside() {
+        Position position = robot.getPosition();
+        return position.getX() < 0 || position.getX() >= layout.getMaxX() ||
+               position.getY() < 0 || position.getY() >= layout.getMaxY();
     }
 
     private String waitInstruction() {
@@ -80,32 +74,7 @@ public class RobotApp {
     }
 
     private void draw() {
-        System.out.println("------ Posisi Awal ------");
-        /*
-        Gambar layout:
-        Contoh:
-        - Posisi robot di 1,1
-        - Area permainan luasnya 10,10
-        sehingga gambar layout akan menjadi:
-
-            o*********
-            **********
-            **********
-            **********
-            **********
-            **********
-            **********
-            **********
-            **********
-            **********
-
-            - konfigurasi (icon robot, posisi robot, luas area dan icon area permainan yang tidak ditempati robot) silahkan gunakan prinsip OOP
-            - icon cukup menggunakan karakter yang ada di keyboard.
-         */
-
-        // contoh konfigurasi (inisiasi object robot) posisi awal robot: 1,1
-
-        this.robot = new Robot('o', new Position(0, 0));
+        System.out.println("------ Posisi Terbaru ------");
 
         for (int y = 0; y < layout.getMaxY(); y++) {
             for (int x = 0; x < layout.getMaxX(); x++) {
@@ -117,7 +86,5 @@ public class RobotApp {
             }
             System.out.println();
         }
-
-
     }
 }
